91d4fbfc1859a42193aeb48da77bc281cbf06d1c
ismrnobody <millanquintana9@gmail.com>
2025-06-13 17:16:00 -0400

    asdf

--- pages/index.vue
+++ pages/index.vue
@@ -3,45 +3,17 @@
     <v-img class="banner" :src="merchant.banner">
       <v-card class="fix mx-auto" flat>
         <!-- <h1>The Taste of Your Celebrations</h1> -->
-        <img width="60%" class="mt-7" :src="merchant.logo" alt="">
+        <img width="60%" class="mt-7" :src="merchant.logo" alt="" />
         <v-col sm="12" cols="9" class="mx-auto">
-          <p class="orange--text">Lunes a Domingo <br> de 10:00am a 10:00pm</p>
-        <v-btn outlined color="" to="/menu" class="mb-5">Ver Menu</v-btn>
+          <p class="orange--text">
+            Lunes a Domingo <br />
+            de 10:00am a 10:00pm
+          </p>
+          <v-btn outlined color="" to="/menu" class="mb-5">Ver Menu</v-btn>
           <h2 class="beige--text spaci">Llegamos a la puerta de tu casa</h2>
         </v-col>
       </v-card>
     </v-img>
-    <v-container>
-      <div class="text-center pad">
-        <v-card max-width="700" flat class="transparent mx-auto mb-5">
-          <h3 class="siz">Nuestra Exquisita Variedad de Sabores</h3>
-          <p>
-            Cada bocado es una explosión de sabores frescos y auténticos, con los ingredientes más selectos.
-          </p>
-        </v-card>
-        <v-row justify="center">
-          <v-col v-for="(promo, i) in promos" :key="i" sm="4" cols>
-            <CardsHome :promo="promo" />
-          </v-col>
-        </v-row>
-      </div>
-    </v-container>
-    <ParallaxHome />
-    <v-container fluid>
-      <div>
-        <BannerHome />
-      </div>
-      <v-row justify="center" class="contacto">
-        <v-col class="my-auto" sm="4" cols>
-          <v-img
-            width="150"
-            class="mx-auto"
-            :src="merchant.logo">
-          </v-img>
-        </v-col>
-        <Contact />
-      </v-row>
-    </v-container>
   </div>
 </template>
 
@@ -56,12 +28,12 @@ export default {
     },
     promos() {
       return this.$store.state.merchant.promos
-    }
+    },
   },
   created() {
     this.$store.dispatch('setMerchant')
     // this.$store.dispatch('setProducts')
-  }
+  },
 }
 </script>
 
@@ -74,7 +46,7 @@ export default {
 }
 .fix {
   backdrop-filter: blur(20px);
-  background-color:  var(--v);
+  background-color: var(--v);
 }
 .pad {
   padding-top: 80px;
@@ -84,7 +56,7 @@ export default {
   padding-bottom: 80px;
   margin-top: 180px;
   /* background: rgb(255,0,0); */
-/* background: linear-gradient(180deg, rgb(255, 170, 0) 0%, rgb(210, 123, 0) 20%, rgba(0,0,0,1) 70%); */
+  /* background: linear-gradient(180deg, rgb(255, 170, 0) 0%, rgb(210, 123, 0) 20%, rgba(0,0,0,1) 70%); */
 }
 h1 {
   font-size: 4rem;
